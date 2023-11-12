68bec3df21c798c48fde1d8c6a81a3ee156e0a0a
Victor Newton <victornewton58@gmail.com>
2023-11-11 22:23:25 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿// ACERTE O NÚMERO
+// Projeto que tem em mente gerar um número aleatória a cada execução,
+// e fazer com que o usuário acerte o número que foi gerado através de dicas.
+
+Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+do {
+    
+    Console.WriteLine("Digite um numero de 1 a 100: ");
+    
+    int chute = int.Parse(Console.ReadLine());
+
+    if(chute == numeroSecreto){
+        Console.WriteLine("Número correto");
+        break;
+    } else if(chute < numeroSecreto){
+        Console.WriteLine("O número secreto é maior");
+    } else{
+        Console.WriteLine("O número secreto é menor");
+    }
+
+}while(true);
+
+Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
+
